03853cdc55d1c9ff99e3f41adfe58f0ca3634d9c
张明飞 <mingfei.zhang@chinaredstar.com>
2018-12-10 09:48:06 +0800

    提交

---
+++ day01/src/com/mfzhang/day20181205/MapTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author mingfei.z 2018年12月5日 下午4:49:12
+ */
+package com.mfzhang.day20181205;
+
+/**
+ * 
+ * @author mingfei.z
+ */
+public class MapTest {
+
+	public static void main(String[] args) {
+		InterTest.get();
+	}
+
+}
